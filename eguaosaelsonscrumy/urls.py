38a7b976ecb34cad27f217886e8575702eb8a5f5
Elson Eguaosa <mac@Macs-MacBook-Pro.local>
2020-09-24 05:37:05 +0100

    first template

--- eguaosaelsonscrumy/urls.py
+++ eguaosaelsonscrumy/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('movegoal/<int:goal_id>', views.move_goal, name='move_goal'),
+    path('addgoal', views.add_goal, name='add_goal'),
+    path('home', views.home, name='home'),
 ]
--- eguaosaelsonscrumy/views.py
+++ eguaosaelsonscrumy/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 from .models import GoalStatus, ScrumyGoals, ScrumyHistory
+from django.contrib.auth.models import User
 
 # Create your views here.
+random_numbers = []
+from random import randint
+def get_val():
+    val = randint(1000, 9999)
+    if val not in random_numbers:
+        random_numbers.append(val)
+        return val
+    else:
+        get_val()
 
 def index(request):
     goal = ScrumyGoals.objects.filter(goal_name='Learn Django')
@@ -12,3 +23,21 @@ def move_goal(request,goal_id):
     goal = ScrumyGoals.objects.get(goal_id = goal_id)
     return HttpResponse(goal)
 
+def add_goal(request):
+    status = GoalStatus.objects.get(status_name='Weekly Goal')
+    user = User.objects.get(username='louis')
+    newgoal = ScrumyGoals(goal_name='Keep Learning Django', goal_id=get_val(),
+                         created_by='Louis', moved_by='Louis', owner='Louis',
+                         goal_status=status, user=user)
+    newgoal.save()
+    return HttpResponse('New Goal Created')
+
+def home(request):
+    eachgoal = ScrumyGoals.objects.filter(goal_name='Keep Learning Django')
+    goal_a = ScrumyGoals.objects.get(goal_name='Learn Django')
+    context = {'goal_name': goal_a.goal_name,
+               'goal_id': goal_a.goal_id,
+               'user': goal_a.user,
+               }
+    output = ', '.join([e.goal_name for e in eachgoal])
+    return render(request, 'eguaosaelsonscrumy/home.html', context)
